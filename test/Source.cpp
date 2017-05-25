8b1dab7f8f604f9ccdc1f8f6953d549a3f1becfe
Viktor Basanets <vbasanec@gmail.com>
2017-05-25 15:59:07 +0300

    init

--- test/Source.cpp
+++ test/Source.cpp
@@ -1,6 +1,244 @@
+//
+// chat_client.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2017 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
 #include <iostream>
-#include <boost\asio.hpp>
-int main()
+#include <thread>
+#include <boost/asio.hpp>
+
+#pragma comment(lib, "libeay32.lib")
+#pragma comment(lib, "ssleay32.lib")
+
+class chat_message
 {
+public:
+	enum { header_length = 4 };
+	enum { max_body_length = 512 };
+
+	chat_message()
+		: body_length_(0)
+	{
+	}
+
+	const char* data() const
+	{
+		return data_;
+	}
+
+	char* data()
+	{
+		return data_;
+	}
+
+	std::size_t length() const
+	{
+		return header_length + body_length_;
+	}
+
+	const char* body() const
+	{
+		return data_ + header_length;
+	}
+
+	char* body()
+	{
+		return data_ + header_length;
+	}
+
+	std::size_t body_length() const
+	{
+		return body_length_;
+	}
+
+	void body_length(std::size_t new_length)
+	{
+		body_length_ = new_length;
+		if (body_length_ > max_body_length)
+			body_length_ = max_body_length;
+	}
+
+	bool decode_header()
+	{
+		char header[header_length + 1] = "";
+		strncat_s(header, data_, header_length);
+		body_length_ = std::atoi(header);
+		if (body_length_ > max_body_length)
+		{
+			body_length_ = 0;
+			return false;
+		}
+		return true;
+	}
+
+	void encode_header()
+	{
+		char header[header_length + 1] = "";
+		sprintf_s(header, "%4d", static_cast<int>(body_length_));
+		std::memcpy(data_, header, header_length);
+	}
+
+private:
+	char data_[header_length + max_body_length];
+	std::size_t body_length_;
+};
+
+using boost::asio::ip::tcp;
+
+typedef std::deque<chat_message> chat_message_queue;
+
+class chat_client
+{
+public:
+	chat_client(boost::asio::io_service& io_service,
+		tcp::resolver::iterator endpoint_iterator)
+		: io_service_(io_service),
+		socket_(io_service)
+	{
+		do_connect(endpoint_iterator);
+	}
+
+	void write(const chat_message& msg)
+	{
+		io_service_.post(
+			[this, msg]()
+		{
+			bool write_in_progress = !write_msgs_.empty();
+			write_msgs_.push_back(msg);
+			if (!write_in_progress)
+			{
+				do_write();
+			}
+		});
+	}
+
+	void close()
+	{
+		io_service_.post([this]() { socket_.close(); });
+	}
+
+private:
+	void do_connect(tcp::resolver::iterator endpoint_iterator)
+	{
+		boost::asio::async_connect(socket_, endpoint_iterator,
+			[this](boost::system::error_code ec, tcp::resolver::iterator)
+		{
+			if (!ec)
+			{
+				do_read_header();
+			}
+		});
+	}
+
+	void do_read_header()
+	{
+		boost::asio::async_read(socket_,
+			boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+			[this](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec && read_msg_.decode_header())
+			{
+				do_read_body();
+			}
+			else
+			{
+				socket_.close();
+			}
+		});
+	}
+
+	void do_read_body()
+	{
+		boost::asio::async_read(socket_,
+			boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+			[this](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec)
+			{
+				std::cout.write(read_msg_.body(), read_msg_.body_length());
+				std::cout << "\n";
+				do_read_header();
+			}
+			else
+			{
+				socket_.close();
+			}
+		});
+	}
+
+	void do_write()
+	{
+		boost::asio::async_write(socket_,
+			boost::asio::buffer(write_msgs_.front().data(),
+				write_msgs_.front().length()),
+			[this](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec)
+			{
+				write_msgs_.pop_front();
+				if (!write_msgs_.empty())
+				{
+					do_write();
+				}
+			}
+			else
+			{
+				socket_.close();
+			}
+		});
+	}
+
+private:
+	boost::asio::io_service& io_service_;
+	tcp::socket socket_;
+	chat_message read_msg_;
+	chat_message_queue write_msgs_;
+};
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc != 3)
+		{
+			std::cerr << "Usage: chat_client <host> <port>\n";
+			return 1;
+		}
+
+		boost::asio::io_service io_service;
+
+		tcp::resolver resolver(io_service);
+		auto endpoint_iterator = resolver.resolve({ argv[1], argv[2] });
+		chat_client c(io_service, endpoint_iterator);
+
+		std::thread t([&io_service]() { io_service.run(); });
+
+		char line[chat_message::max_body_length + 1];
+		while (std::cin.getline(line, chat_message::max_body_length + 1))
+		{
+			chat_message msg;
+			msg.body_length(std::strlen(line));
+			std::memcpy(msg.body(), line, msg.body_length());
+			msg.encode_header();
+			c.write(msg);
+		}
+
+		c.close();
+		t.join();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
 	return 0;
 }
