736e3ce6aceea4b296459988ff08e74a6433246e
Viktor Basanets <vbasanec@gmail.com>
2017-05-26 11:42:07 +0300

    init

--- test/Source.cpp
+++ test/Source.cpp
@@ -1,202 +1,24 @@
 //
-// chat_client.cpp
+// sync_client.cpp
 // ~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2017 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <deque>
 #include <iostream>
-#include <thread>
+#include <istream>
+#include <ostream>
+#include <string>
 #include <boost/asio.hpp>
 
 #pragma comment(lib, "libeay32.lib")
 #pragma comment(lib, "ssleay32.lib")
 
-class chat_message
-{
-public:
-	enum { header_length = 4 };
-	enum { max_body_length = 512 };
-
-	chat_message()
-		: body_length_(0)
-	{
-	}
-
-	const char* data() const
-	{
-		return data_;
-	}
-
-	char* data()
-	{
-		return data_;
-	}
-
-	std::size_t length() const
-	{
-		return header_length + body_length_;
-	}
-
-	const char* body() const
-	{
-		return data_ + header_length;
-	}
-
-	char* body()
-	{
-		return data_ + header_length;
-	}
-
-	std::size_t body_length() const
-	{
-		return body_length_;
-	}
-
-	void body_length(std::size_t new_length)
-	{
-		body_length_ = new_length;
-		if (body_length_ > max_body_length)
-			body_length_ = max_body_length;
-	}
-
-	bool decode_header()
-	{
-		char header[header_length + 1] = "";
-		strncat_s(header, data_, header_length);
-		body_length_ = std::atoi(header);
-		if (body_length_ > max_body_length)
-		{
-			body_length_ = 0;
-			return false;
-		}
-		return true;
-	}
-
-	void encode_header()
-	{
-		char header[header_length + 1] = "";
-		sprintf_s(header, "%4d", static_cast<int>(body_length_));
-		std::memcpy(data_, header, header_length);
-	}
-
-private:
-	char data_[header_length + max_body_length];
-	std::size_t body_length_;
-};
-
 using boost::asio::ip::tcp;
 
-typedef std::deque<chat_message> chat_message_queue;
-
-class chat_client
-{
-public:
-	chat_client(boost::asio::io_service& io_service, tcp::resolver::iterator endpoint_iterator)
-		: io_service_(io_service), socket_(io_service)
-	{
-		do_connect(endpoint_iterator);
-	}
-
-	void write(const chat_message& msg)
-	{
-		io_service_.post(
-			[this, msg]() {
-				bool write_in_progress = !write_msgs_.empty();
-				write_msgs_.push_back(msg);
-				if (!write_in_progress) { do_write(); }
-			});
-	}
-
-	void close()
-	{
-		io_service_.post([this]() { socket_.close(); });
-	}
-
-private:
-	void do_connect(tcp::resolver::iterator endpoint_iterator)
-	{
-		boost::asio::async_connect(socket_, endpoint_iterator,
-			[this](boost::system::error_code ec, tcp::resolver::iterator)
-		{
-			if (!ec)
-			{
-				do_read_header();
-			}
-		});
-	}
-
-	void do_read_header()
-	{
-		boost::asio::async_read(socket_,
-			boost::asio::buffer(read_msg_.data(), chat_message::header_length),
-			[this](boost::system::error_code ec, std::size_t /*length*/)
-		{
-			if (!ec && read_msg_.decode_header())
-			{
-				do_read_body();
-			}
-			else
-			{
-				socket_.close();
-			}
-		});
-	}
-
-	void do_read_body()
-	{
-		boost::asio::async_read(socket_,
-			boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
-			[this](boost::system::error_code ec, std::size_t /*length*/)
-		{
-			if (!ec)
-			{
-				std::cout.write(read_msg_.body(), read_msg_.body_length());
-				std::cout << "\n";
-				do_read_header();
-			}
-			else
-			{
-				socket_.close();
-			}
-		});
-	}
-
-	void do_write()
-	{
-		boost::asio::async_write(socket_,
-			boost::asio::buffer(write_msgs_.front().data(),
-				write_msgs_.front().length()),
-			[this](boost::system::error_code ec, std::size_t /*length*/)
-		{
-			if (!ec)
-			{
-				write_msgs_.pop_front();
-				if (!write_msgs_.empty())
-				{
-					do_write();
-				}
-			}
-			else
-			{
-				socket_.close();
-			}
-		});
-	}
-
-private:
-	boost::asio::io_service& io_service_;
-	tcp::socket socket_;
-	chat_message read_msg_;
-	chat_message_queue write_msgs_;
-};
 
 int main(int argc, char* argv[])
 {
@@ -204,34 +26,89 @@ int main(int argc, char* argv[])
 	{
 		if (argc != 3)
 		{
-			std::cerr << "Usage: chat_client <host> <port>\n";
+			std::cout << "Usage: sync_client <server> <path>\n";
+			std::cout << "Example:\n";
+			std::cout << "  sync_client www.boost.org /LICENSE_1_0.txt\n";
 			return 1;
 		}
 
 		boost::asio::io_service io_service;
 
+		// Get a list of endpoints corresponding to the server name.
 		tcp::resolver resolver(io_service);
-		auto endpoint_iterator = resolver.resolve({ argv[1], argv[2] });
-		chat_client c(io_service, endpoint_iterator);
+		tcp::resolver::query query(argv[1], "http");
+		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+		tcp::resolver::iterator end;
 
-		std::thread t([&io_service]() { io_service.run(); });
-
-		char line[chat_message::max_body_length + 1];
-		while (std::cin.getline(line, chat_message::max_body_length + 1))
+		// Try each endpoint until we successfully establish a connection.
+		tcp::socket socket(io_service);
+		boost::system::error_code error = boost::asio::error::host_not_found;
+		while (error && endpoint_iterator != end)
 		{
-			chat_message msg;
-			msg.body_length(std::strlen(line));
-			std::memcpy(msg.body(), line, msg.body_length());
-			msg.encode_header();
-			c.write(msg);
+			socket.close();
+			socket.connect(*endpoint_iterator++, error);
+		}
+		if (error)
+			throw boost::system::system_error(error);
+
+		// Form the request. We specify the "Connection: close" header so that the
+		// server will close the socket after transmitting the response. This will
+		// allow us to treat all data up until the EOF as the content.
+		boost::asio::streambuf request;
+		std::ostream request_stream(&request);
+		request_stream << "GET " << argv[2] << " HTTP/1.0\r\n";
+		request_stream << "Host: " << argv[1] << "\r\n";
+		request_stream << "Accept: */*\r\n";
+		request_stream << "Connection: close\r\n\r\n";
+
+		// Send the request.
+		boost::asio::write(socket, request);
+
+		// Read the response status line.
+		boost::asio::streambuf response;
+		boost::asio::read_until(socket, response, "\r\n");
+
+		// Check that response is OK.
+		std::istream response_stream(&response);
+		std::string http_version;
+		response_stream >> http_version;
+		unsigned int status_code;
+		response_stream >> status_code;
+		std::string status_message;
+		std::getline(response_stream, status_message);
+		if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+		{
+			std::cout << "Invalid response\n";
+			return 1;
+		}
+		if (status_code != 200)
+		{
+			std::cout << "Response returned with status code " << status_code << "\n";
+			return 1;
 		}
 
-		c.close();
-		t.join();
+		// Read the response headers, which are terminated by a blank line.
+		boost::asio::read_until(socket, response, "\r\n\r\n");
+
+		// Process the response headers.
+		std::string header;
+		while (std::getline(response_stream, header) && header != "\r")
+			std::cout << header << "\n";
+		std::cout << "\n";
+
+		// Write whatever content we already have to output.
+		if (response.size() > 0)
+			std::cout << &response;
+
+		// Read until EOF, writing data to output as we go.
+		while (boost::asio::read(socket, response, boost::asio::transfer_at_least(1), error))
+			std::cout << &response;
+		if (error != boost::asio::error::eof)
+			throw boost::system::system_error(error);
 	}
 	catch (std::exception& e)
 	{
-		std::cerr << "Exception: " << e.what() << "\n";
+		std::cout << "Exception: " << e.what() << "\n";
 	}
 
 	return 0;
--- test/main.cpp
+++ test/main.cpp
@@ -12,13 +12,16 @@
 #include <istream>
 #include <ostream>
 #include <string>
+#include <fstream>
 #include <boost/asio.hpp>
 
 #pragma comment(lib, "libeay32.lib")
 #pragma comment(lib, "ssleay32.lib")
 
 using boost::asio::ip::tcp;
-std::string client(std::string port, std::string host, std::string link)
+
+std::string client(const std::string & port,
+	const std::string & host, const std::string & link)
 {
 	boost::asio::io_service io_service;
 	// Get a list of endpoints corresponding to the server name.
@@ -64,120 +67,40 @@ std::string client(std::string port, std::string host, std::string link)
 	std::string status_message;
 	std::getline(response_stream, status_message);
 	if (!response_stream || http_version.substr(0, 5) != "HTTP/")
-	{
-		std::cout << "Invalid response\n";
-		throw 1;
-	}
+		throw "Invalid response";
 	if (status_code != 200)
-	{
-		std::cout << "Response returned with status code " << status_code << "\n";
-		throw 1;
-	}
+		throw "Response returned with status code " + status_code;
 
 	// Read the response headers, which are terminated by a blank line.
 	boost::asio::read_until(socket, response, "\r\n\r\n");
 
 	// Process the response headers.
 	std::string header;
-	while (std::getline(response_stream, header) && header != "\r")
-		std::cout << header << "\n";
-	std::cout << "\n";
-
-	// Write whatever content we already have to output.
-	if (response.size() > 0)
-		std::cout << &response;
+	std::ostringstream all_responce;
+	while (std::getline(response_stream, header))
+		all_responce << header << "\n";
+	all_responce << "-------------------------------------------------------------------------------------------------------\n";
 
 	// Read until EOF, writing data to output as we go.
 	while (boost::asio::read(socket, response, boost::asio::transfer_at_least(1), error))
-		std::cout << &response;
+	{
+		std::getline(response_stream, header);
+		all_responce << header << "\n";
+	}
+	all_responce << "-------------------------------------------------------------------------------------------------------\n";
 	if (error != boost::asio::error::eof)
 		throw boost::system::system_error(error);
+	return all_responce.str();
 }
-int main(int argc, char* argv[])
+int main()
 {
+	std::string port = "http";
+	std::string host = "ukraina.ru";
+	std::string link = "/archive/20170502/";
+	std::ofstream out("d:/test_out.txt");
 	try
 	{
-		if (argc != 3)
-		{
-			std::cout << "Usage: sync_client <server> <path>\n";
-			std::cout << "Example:\n";
-			std::cout << "  sync_client www.boost.org /LICENSE_1_0.txt\n";
-			return 1;
-		}
-
-		boost::asio::io_service io_service;
-
-		// Get a list of endpoints corresponding to the server name.
-		tcp::resolver resolver(io_service);
-		tcp::resolver::query query(argv[1], "http");
-		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-		tcp::resolver::iterator end;
-
-		// Try each endpoint until we successfully establish a connection.
-		tcp::socket socket(io_service);
-		boost::system::error_code error = boost::asio::error::host_not_found;
-		while (error && endpoint_iterator != end)
-		{
-			socket.close();
-			socket.connect(*endpoint_iterator++, error);
-		}
-		if (error)
-			throw boost::system::system_error(error);
-
-		// Form the request. We specify the "Connection: close" header so that the
-		// server will close the socket after transmitting the response. This will
-		// allow us to treat all data up until the EOF as the content.
-		boost::asio::streambuf request;
-		std::ostream request_stream(&request);
-		request_stream << "GET " << argv[2] << " HTTP/1.0\r\n";
-		request_stream << "Host: " << argv[1] << "\r\n";
-		request_stream << "Accept: */*\r\n";
-		request_stream << "Connection: close\r\n\r\n";
-
-		// Send the request.
-		boost::asio::write(socket, request);
-
-		// Read the response status line.
-		boost::asio::streambuf response;
-		boost::asio::read_until(socket, response, "\r\n");
-
-		// Check that response is OK.
-		std::istream response_stream(&response);
-		std::string http_version;
-		response_stream >> http_version;
-		unsigned int status_code;
-		response_stream >> status_code;
-		std::string status_message;
-		std::getline(response_stream, status_message);
-		if (!response_stream || http_version.substr(0, 5) != "HTTP/")
-		{
-			std::cout << "Invalid response\n";
-			return 1;
-		}
-		if (status_code != 200)
-		{
-			std::cout << "Response returned with status code " << status_code << "\n";
-			return 1;
-		}
-
-		// Read the response headers, which are terminated by a blank line.
-		boost::asio::read_until(socket, response, "\r\n\r\n");
-
-		// Process the response headers.
-		std::string header;
-		while (std::getline(response_stream, header) && header != "\r")
-			std::cout << header << "\n";
-		std::cout << "\n";
-
-		// Write whatever content we already have to output.
-		if (response.size() > 0)
-			std::cout << &response;
-
-		// Read until EOF, writing data to output as we go.
-		while ( boost::asio::read(socket, response, boost::asio::transfer_at_least(1), error) )
-			std::cout << &response;
-		if (error != boost::asio::error::eof)
-			throw boost::system::system_error(error);
+		out << client(port, host, link) << std::endl;
 	}
 	catch (std::exception& e)
 	{
