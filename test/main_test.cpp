607ad32883e8cfc5c1e3409b5c698bd633bbfee2
Viktor Basanets <vbasanec@gmail.com>
2017-05-31 16:00:10 +0300

    init

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -25,8 +25,6 @@
 #pragma comment(lib, "ssleay32.lib")
 #pragma comment(lib, "libiconv.lib")
 
-using boost::asio::ip::tcp;
-
 class LinksClass
 {
 	std::string _port;
@@ -150,13 +148,15 @@ std::string monthToNumber(const std::string & month)
 }
 std::string client(const std::string & host, const std::string & link)
 {
+	using boost::asio::ip::tcp;
+
 	boost::asio::io_service io_service;
 	// Get a list of endpoints corresponding to the server name.
 	tcp::resolver resolver(io_service);
 	tcp::resolver::query query(host, "http");
 	tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 	tcp::resolver::iterator end;
-
+	
 	// Try each endpoint until we successfully establish a connection.
 	tcp::socket socket(io_service);
 	boost::system::error_code error = boost::asio::error::host_not_found;
@@ -168,19 +168,19 @@ std::string client(const std::string & host, const std::string & link)
 	if (error)
 		throw boost::system::system_error(error);
 
-	// Form the request. We specify the "Connection: close" header so that the
-	// server will close the socket after transmitting the response. This will
-	// allow us to treat all data up until the EOF as the content.
+	/*	Form the request. We specify the "Connection: close" header so that the
+	server will close the socket after transmitting the response. This will
+	allow us to treat all data up until the EOF as the content.	*/
 	boost::asio::streambuf request;
 	std::ostream request_stream(&request);
 	request_stream << "GET " << link << " HTTP/1.0\r\n";
 	request_stream << "Host: " << host << "\r\n";
+	request_stream << "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:47.0) Gecko/20100101 Firefox/47.0\r\n";
 	request_stream << "Accept: */*\r\n";
 	request_stream << "Connection: close\r\n\r\n";
-
+	
 	// Send the request.
 	boost::asio::write(socket, request);
-
 	// Read the response status line.
 	boost::asio::streambuf response;
 	boost::asio::read_until(socket, response, "\r\n");
@@ -189,42 +189,99 @@ std::string client(const std::string & host, const std::string & link)
 	std::istream response_stream(&response);
 	std::string http_version;
 	response_stream >> http_version;
+
 	unsigned int status_code;
 	response_stream >> status_code;
+
 	std::string status_message;
 	std::getline(response_stream, status_message);
+	
 	if (!response_stream || http_version.substr(0, 5) != "HTTP/")
-		throw "Invalid response";
+		throw std::string("Invalid response") + http_version;
 	if (status_code != 200)
-		throw "Response returned with status code " + status_code;
+		throw std::string("Response returned with status code ") + toStr(status_code);
 
 	// Read the response headers, which are terminated by a blank line.
 	boost::asio::read_until(socket, response, "\r\n\r\n");
-
 	// Process the response headers.
 	std::string header;
 	std::ostringstream all_responce;
-	/*/
-	while (std::getline(response_stream, header) && header != "\r")
-		all_responce << header << "\n";
-	//all_responce << "-------------------------------------------------------------------------------------------------------\n";
-	if (response.size() > 0)
-		all_responce << &response;
-		*/
+	
 	// Read until EOF, writing data to output as we go.
-
 	while (boost::asio::read(socket, response, boost::asio::transfer_at_least(1), error))
 		all_responce << &response << "\n";
-
-	//all_responce << "-------------------------------------------------------------------------------------------------------\n";
-
 	if (error != boost::asio::error::eof)
 		throw boost::system::system_error(error);
 	return all_responce.str();
 }
-std::string client(const std::string & host, const std::string & link, const std::string & port)
+std::string client(const std::string & host, const std::string & link,
+	const std::string & port)
 {
+	using boost::asio::ip::tcp;
+	using boost::asio::ssl::context;
+	using boost::asio::ssl::stream;
+	using boost::asio::ssl::rfc2818_verification;
 
+	boost::asio::io_service io_service;
+	// Get a list of endpoints corresponding to the server name.
+	tcp::resolver resolver(io_service);
+	tcp::resolver::query query(host, port);
+	tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+	tcp::resolver::iterator end;
+
+	// Try each endpoint until we successfully establish a connection.
+	context ctx(context::sslv23);
+	ctx.set_default_verify_paths();
+	stream<tcp::socket> socket(io_service, ctx);
+
+	/*	Form the request. We specify the "Connection: close" header so that the
+	server will close the socket after transmitting the response. This will
+	allow us to treat all data up until the EOF as the content.	*/
+	boost::asio::streambuf request;
+	std::ostream request_stream(&request);
+	request_stream << "GET " << link << " HTTP/1.0\r\n";
+	request_stream << "Host: " << host << "\r\n";
+	request_stream << "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:47.0) Gecko/20100101 Firefox/47.0\r\n";
+	request_stream << "Accept: */*\r\n";
+	request_stream << "Connection: close\r\n\r\n";
+	
+	connect(socket.lowest_layer(), endpoint_iterator);
+	socket.set_verify_mode(boost::asio::ssl::verify_none);
+	socket.set_verify_callback(rfc2818_verification(host));
+	socket.handshake(stream<tcp::socket>::client);
+
+	// Send the request.
+	write(socket, request);
+
+	// Read the response status line.
+	boost::asio::streambuf response;
+	read_until(socket, response, "\r\n");
+
+	// Check that response is OK.
+	std::istream response_stream(&response);
+	std::string http_version;
+	response_stream >> http_version;
+	unsigned int status_code;
+	response_stream >> status_code;
+
+	std::string status_message;
+	std::getline(response_stream, status_message);
+	if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+		throw std::string("Invalid response") + http_version;
+	if (status_code != 200)
+		throw std::string("Response returned with status code ") + toStr(status_code);
+	
+	// Read the response headers, which are terminated by a blank line.
+	read_until(socket, response, "\r\n\r\n");
+	// Process the response headers.
+	std::string header;
+	std::ostringstream all_responce;
+	// Read until EOF, writing data to output as we go.
+	boost::system::error_code error;
+	int count = 0;
+	while (boost::asio::read(socket, response, boost::asio::transfer_at_least(1), error))
+		all_responce << &response << "\n";
+	return all_responce.str();
 }
 std::string convertAndEncoding(const std::string& data, const std::string& from, const std::string& to)
 {
@@ -267,6 +324,7 @@ void decodingInaSingleString(std::string & row)
 {
 	std::string temp, str;
 	std::istringstream iss(row);
+	
 	std::ofstream out("d:/log.txt");
 	while (iss >> temp)
 	{
@@ -306,14 +364,12 @@ void testOut(const std::string & str, const std::string & fileName)
 	std::ofstream out(fileName.c_str());
 	out << str << std::endl;
 }
-/*
-****************************************************
-argv[1] - name Host
-argv[2] - Month
-argv[3] - year
 
-****************************************************
-*/
+/*****************************************************
+				argv[1] - name Host
+				argv[2] - Month
+				argv[3] - year
+*****************************************************/
 int main(int argc, char * argv[])
 {
 	setlocale(0, "");
@@ -329,6 +385,8 @@ int main(int argc, char * argv[])
 	std::string year(argv[3]);
 	std::string monthNumerical = monthToNumber(month);
 	size_t daysInMonth = countDaysInMonth(month);
+	std::string raw;
+	boost::regex re;
 	std::set<std::string> selection;
 	try
 	{
@@ -341,24 +399,26 @@ int main(int argc, char * argv[])
 				"&date_to=" +
 				year + "-" + monthNumerical + "-" + toStr(daysInMonth) +
 				"&query%5B%5D=%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0&limit=" +
-				toStr(limit);
+				toStr(limit);	
 			std::string prefix = "&offset=";
-			
-			std::string raw = client(host, sufix + prefix + toStr(0));
+		
+			raw = client(host, sufix + prefix + toStr(0));
 			decodingInaSingleString(raw);
 			
-			boost::regex re(R"#(<div\sclass=\"search__results\">.*?<span>(.*?)</span>)#");
+			re = boost::regex(R"#(<div\sclass=\"search__results\">.*?<span>(.*?)</span>)#");
 			raw = boost::regex_replace(raw, re, "$1", boost::regex_constants::format_no_copy);
 			
 			int sumAllPosts = pullNumber<int>(raw);
 			int sumRequests = integerPath(sumAllPosts, limit);
-			
 			srand((unsigned int)time(0));
-			for (int i = 0; i < sumRequests; ++i)
+			raw.clear();
+			for (int i = 1; i < sumRequests; ++i)
 			{
-				raw = client(host, sufix + prefix + toStr(i*limit));
-				timeDelay(rand() / 5000);
+				std::cout << "GET requests --- " << i << " --- \n";
+				raw += client(host, sufix + prefix + toStr(i*limit));
+				timeDelay(rand() / 50000);
 			}
+			decodingInaSingleString(raw);
 
 			testOut(raw, "d:/outFile.txt");
 		}
@@ -369,7 +429,31 @@ int main(int argc, char * argv[])
 		}
 		else if (host == "russian.rt.com")
 		{
+			int limit = 10;
+			std::string sufix = "/search?q=%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0&type=News&df=0" +
+				toStr(1) + "-" + monthNumerical + "-" + year +
+				"&dt=" +
+				toStr(daysInMonth) + "-" + monthNumerical + "-" + year +
+				"&page=";
+			std::string prefix = "";
 			
+			raw = client(host, sufix + prefix + toStr(1), "https");
+			decodingInaSingleString(raw);
+			re = boost::regex(R"#(<p class=\"search-serp__total\">(.*?)</p>)#");
+			raw = boost::regex_replace(raw, re, "$1", boost::regex_constants::format_no_copy);
+			
+			int sumAllPosts = pullNumber<int>(raw);
+			int sumRequests = integerPath(sumAllPosts, limit);
+			srand((unsigned int)time(0));
+			raw.clear();
+			for (int i = 1; i < sumRequests; ++i)
+			{
+				raw += client(host, sufix + prefix + toStr(i), "https");
+				timeDelay(rand() / 5000);
+			}
+			decodingInaSingleString(raw);
+
+			testOut(raw, "d:/outFile.txt");
 		}
 		else
 		{
@@ -379,6 +463,10 @@ int main(int argc, char * argv[])
 	catch (std::exception& e)
 	{
 		std::cout << "Exception: " << e.what() << "\n";
+	}
+	catch (std::string e)
+	{
+		std::cout << "String exception: " << e << "\n";
 	}
 
 	return 0;
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <ctime>
+#include <algorithm>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/regex.hpp>
+#include <iconv.h>
+#pragma comment(lib, "libeay32.lib")
+#pragma comment(lib, "ssleay32.lib")
+#pragma comment(lib, "libiconv.lib")
 
 int main()
 {
 	
-
+	srand((unsigned int)time(0));
+	for (int i = 1; i != 100; i++)
+		std::cout << rand()/5000 << " ";
+	std::cout << std::endl;
+	std::cout << CLOCKS_PER_SEC << std::endl;
+	
 	return 0;
 }
